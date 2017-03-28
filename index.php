7352b4414e337f066ac9a55fcc4009587a88ee68
Carl Wingårdh <c.a.wingardh@gmail.com>
2017-03-28 12:31:11 +0200

    Added two new logo animations and begun layout of rest of site.
    The two new logo animations are blast-apart and video-mask.
    The video mask doesn't have a seperate .svg-file, but does, however need a .mp4-file which is not included in the commit. It will soon be made available online so it doesn't have to be collected from the server itself.
    The layout of the rest of the side now includes a bar at the bottom with a lighter background to draw the eye to the rest of the site.
    There is also a link to the CSS in the nav-bar. This is temporary and only for development.

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <head>
-<link href="style.css" rel="stylesheet" type="text/css" media="screen">
-<link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 
 <body>
     <div class="vspacer-1"> </div>
     <div class="nav-bar">
-	<div class="nav-bar-item-right">
-	    <p class="centered"> hello </p>
-	</div>
+	<a href="style.css"> 
+	    <div class="nav-bar-item-right">
+		<p class="centered"> CSS  </p>
+	    </div>
+	</a>
 	<div class="nav-bar-item-right">
 	    <p class="centered"> hell </p>
 	</div>
@@ -21,36 +24,50 @@
 	    ?>
 	</div>
     </div>
-    
-    <div class="container-1 centered full-page">
-    <div id="title-card" class="container-2">
-	<?php
-     switch(rand(1, 5)) {
-	 case 1:
-	     include("resources/images/wingardh-logo/wingardh-logo.svg");
-	     break;
-	 case 2:
-	     include("resources/images/wingardh-logo/wingardh-logo-wave.svg");
-	     break;
-	 case 3:
-	     include("resources/images/wingardh-logo/wingardh-logo-neon.svg");
-	     break;
-	 case 4:
-	     include("resources/images/wingardh-logo/wingardh-logo-draw-in.svg");
-	     break;
-	 case 5:
-	     include("resources/images/wingardh-logo/wingardh-logo-toggle-colors.svg");
-	     break;
-	 default:
-	     include("resources/images/wingardh-logo/wingardh-logo.svg");
-	     }
-	?>
-	
-    <div id="banner">
-	<h1 class="color-fg-primary">That Vineyard</h1>
-    </div> <!-- #banner -->
-    </div> <!-- .sixteenbynine .centered -->
+
+    <div class="container-1 centered">
+	<div id="title-card" class="container-2 sixteenbynine centered">
+	    <div id="logo" class="sixteenbynine">
+		<?php
+		switch(rand(6, 7)) {
+		    case 1:
+			include("resources/images/wingardh-logo/wingardh-logo-blast-apart.svg");
+			break;
+		    case 2:
+			include("resources/images/wingardh-logo/wingardh-logo-wave.svg");
+			break;
+		    case 3:
+			include("resources/images/wingardh-logo/wingardh-logo-neon.svg");
+			break;
+		    case 4:
+			include("resources/images/wingardh-logo/wingardh-logo-draw-in.svg");
+			break;
+		    case 5:
+			include("resources/images/wingardh-logo/wingardh-logo-toggle-colors.svg");
+			break;
+		    case 6:
+		?>
+		    <video autoplay="autoplay" muted="muted" preload="auto" loop="loop" style="width: 100%; -webkit-mask-box-image: url(resources/images/wingardh-logo/wingardh-logo.svg) 100% 0% 0% 0%;">
+			<source src="resources/videos/light-effects-animation.mp4" type="video/mp4">
+		    </video>
+		    <?php
+		    break;
+		    default:
+		    include("resources/images/wingardh-logo/wingardh-logo.svg");
+		    }
+		    ?>
+	    </div> <!-- #logo -->
+	    <div id="banner">
+		<h1>That Vineyard</h1>
+	    </div> <!-- #banner -->
+	</div> <!-- .sixteenbynine .centered -->
     </div> <!-- .container-1 -->
+
+    <div class="vspacer-1 bar"> </div>
+    <div class="color-bg-tertiary">
+    <div id="body" class="container-3 centered">
+	
+    </div> <!-- #body -->
+    </div>
 </body>
 
-Web finished at Mon Mar 27 16:25:12
