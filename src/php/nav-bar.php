265bef2d18410c20aa97166d9dd6b96e46c9dad4
Carl Wingårdh <c.a.wingardh@gmail.com>
2017-04-01 23:36:12 +0200

    Changing navbar slightly
    Added background color, made it float above logo. Pushed in the sides a little bit. Made the buttons react to the mouse

--- src/php/nav-bar.php
+++ src/php/nav-bar.php
@@ -2,20 +2,20 @@
 include_once("utils.php");
 
 function generate_nav_bar() {
-    div_open("", "nav-bar");
+    div_open("nav-bar", "");
     generate_nav_bar_button("right", "CSS", "resources/css/style.css");
     generate_nav_bar_button("right", "2", "#");
     generate_nav_bar_button("right", "3", "#");
     generate_nav_bar_image("left", "resources/images/wingardh-logo/wingardh-logo.svg", "/");
-    div_close("", "nav-bar");
+    div_close("nav-bar", "");
 }
 
 function generate_nav_bar_button($align, $text, $url) {
-    div_open_clickable("", "nav-bar-item-" . $align, "$url");
+    div_open_clickable("", "nav-bar-item-" . $align . " nav-bar-item-clickable", "$url");
 
     echo '<p class="centered color-fg-tertiary"> ' . $text . ' </p>';
     
-    div_close_clickable("", "nav-bar-item-". $align);
+    div_close_clickable("", "nav-bar-item-". $align . " nav-bar-item-clickable");
 }
 
 function generate_nav_bar_image($align, $image_url, $url) {
@@ -23,7 +23,7 @@ function generate_nav_bar_image($align, $image_url, $url) {
 
     include($image_url);
     
-    div_close_clickable("", "nav-bar-item-image".$align);
+    div_close_clickable("", "nav-bar-item-image" . $align);
 }
 ?>
 
